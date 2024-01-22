b61c6110cb3a76acf0460011b19f130e446aff1b
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-01-22 18:26:58 +0000

    refactor(srp): standardise parameter datatypes and testing

--- src/srp/srp.js
+++ src/srp/srp.js
@@ -1,18 +1,16 @@
 import * as crypto from "crypto";
-import * as utils from "../utils.js";
 import { getParams } from "./params.js";
+import { hex } from "../utils.js";
 
 export const core = {};
 export const client = {};
 export const server = {};
 
-// TODO: check extra mod N is needed in some places
-// TODO: make function I/O datatypes consistent
-
 // Use 1024-bit group if testing, use 4096-bit group otherwise
 const group = process.env.NODE_ENV === "test" ? 1024 : 4096;
 const params = getParams(group);
 
+// Hashes an arbitrary number of buffers
 function hash(...args) {
   const H = crypto.createHash(params.hash);
   args.forEach((arg) => H.update(arg));
@@ -21,7 +19,7 @@ function hash(...args) {
 
 // Left-pads buffer until its length matches N
 function pad(n) {
-  const len = utils.hex.toBuffer(params.N).length;
+  const len = hex.toBuffer(params.N).length;
   const buf = Buffer.alloc(len - n.length);
   return Buffer.concat([buf, n]);
 }
@@ -44,11 +42,9 @@ function mod(n, d) {
   return ((n % d) + d) % d;
 }
 
-// a^x (mod N)
-// All params are BigInts
+// Modular exponentiation for BigInts: a^x (mod N)
 function mod_exp(a, x, N) {
   if (N === 1n) return 0n;
-  // TODO: assert (N - 1)^2 does not overflow base
   let res = 1n;
   a = mod(a, N);
 
@@ -56,7 +52,6 @@ function mod_exp(a, x, N) {
     if (mod(x, 2n) === 1n) {
       res = mod(res * a, N);
     }
-
     x = x >> 1n;
     a = mod(a ** 2n, N);
   }
@@ -64,94 +59,94 @@ function mod_exp(a, x, N) {
   return res;
 }
 
-core.derive_k = function() {
-  return utils.hex.toBigInt(
-    hash(
-      utils.hex.toBuffer(params.N),
-      pad(utils.hex.toBuffer(params.g))
-    )
+core.derive_k = function () {
+  return hex.toBigInt(
+    hash(hex.toBuffer(params.N), pad(hex.toBuffer(params.g)))
   );
-}
+};
 
 // Used for generating the random values of the private keys a, b.
-// TODO: add test?
-core.get_private_ephemeral_key = function(bitLength = 512) {
+core.get_private_ephemeral_key = function (bitLength = 512) {
   if (bitLength < 256)
-    throw new Error("Must have a bit length of at least the recommended 256-bits.");
-  return utils.hex.toBigInt(crypto.randomBytes(bitLength / 8));
-}
+    throw new Error(
+      "Must have a bit length of at least the recommended 256-bits."
+    );
+  return hex.toBigInt(crypto.randomBytes(bitLength / 8));
+};
 
-core.derive_u = function(A, B) {
-  const u = hash(
-    pad(utils.hex.toBuffer(A)),
-    pad(utils.hex.toBuffer(B))
-  );
+core.derive_u = function (A, B) {
+  const u = hash(pad(hex.toBuffer(A)), pad(hex.toBuffer(B)));
+  return hex.toBigInt(u);
+};
 
-  return utils.hex.toBigInt(u);
-}
+core.derive_K = function (S) {
+  const K = hash(hex.toBuffer(S));
+  return hex.toBigInt(K);
+};
 
-core.derive_K = function(S) {
-  return hash(utils.hex.toBuffer(S));
-}
-
-client.derive_x = function(I, p, s) {
+client.derive_x = function (I, p, s) {
+  const salt = hex.toBuffer(s);
   const user_hash = hash(Buffer.concat([I, Buffer.from(":"), p]));
-  return utils.hex.toBigInt(hash(s, user_hash));
-}
+  return hex.toBigInt(hash(salt, user_hash));
+};
 
-client.derive_v = function(x) {
+client.derive_v = function (x) {
   return mod_exp(params.g, x, params.N);
-}
+};
 
-client.derive_A = function(a) {
+client.derive_A = function (a) {
   return mod_exp(params.g, a, params.N);
-}
+};
 
-client.derive_S = function(k, x, a, B, u) {
+client.derive_S = function (k, x, a, B, u) {
   if (mod(B, params.N) === 0n || u === 0n)
-    throw new Error("The server sent an invalid public ephemeral value, aborting...");
+    throw new Error(
+      "The server sent an invalid public ephemeral value, aborting..."
+    );
 
-  return mod_exp(
-    B - k * mod_exp(params.g, x, params.N),
-    a + u * x,
-    params.N
-  );
-}
+  return mod_exp(B - k * mod_exp(params.g, x, params.N), a + u * x, params.N);
+};
 
-client.derive_K = function(k, x, a, B, u) {
-  const S = utils.hex.toBuffer(client.derive_S(k, x, a, B, u));
-  return hash(S);
-}
+client.derive_K = function (k, x, a, B, u) {
+  const S = hex.toBuffer(client.derive_S(k, x, a, B, u));
+  return hex.toBigInt(hash(S));
+};
 
-client.derive_M1 = function(I, s, A, B, K) {
+client.derive_M1 = function (I, s, A, B, K) {
   const group_hash = buf_xor(
-    hash(utils.hex.toBuffer(params.N)),
-    hash(utils.hex.toBuffer(params.g))
+    hash(hex.toBuffer(params.N)),
+    hash(hex.toBuffer(params.g))
   );
 
-  return utils.hex.toBigInt(hash(
-    group_hash,
-    hash(I),
-    s, A, B, K
-  ));
-}
+  return hex.toBigInt(
+    hash(
+      group_hash,
+      hash(I),
+      hex.toBuffer(s),
+      hex.toBuffer(A),
+      hex.toBuffer(B),
+      hex.toBuffer(K)
+    )
+  );
+};
 
-server.derive_B = function(b, v, k) {
+server.derive_B = function (b, v, k) {
   return (k * v + mod_exp(params.g, b, params.N)) % params.N;
-}
+};
 
-server.derive_S = function(v, A, b, u) {
+server.derive_S = function (v, A, b, u) {
   if (mod(A, params.N) === 0n)
-    throw new Error("The client sent an invalid public ephemeral value, aborting...");
+    throw new Error(
+      "The client sent an invalid public ephemeral value, aborting..."
+    );
   return mod_exp(A * mod_exp(v, u, params.N), b, params.N);
-}
+};
 
-server.derive_K = function(v, A, b, u) {
-  const S = utils.hex.toBuffer(server.derive_S(v, A, b, u));
-  return hash(S);
-}
+server.derive_K = function (v, A, b, u) {
+  const S = hex.toBuffer(server.derive_S(v, A, b, u));
+  return hex.toBigInt(hash(S));
+};
 
-server.derive_M2 = function(A, M1, K) {
-  return utils.hex.toBigInt(hash(A, M1, K));
-}
-
+server.derive_M2 = function (A, M1, K) {
+  return hex.toBigInt(hash(hex.toBuffer(A), hex.toBuffer(M1), hex.toBuffer(K)));
+};
--- tests/srp.test.js
+++ tests/srp.test.js
@@ -1,191 +1,120 @@
 import { describe, expect, test } from "vitest";
 import { core, client, server } from "../src/srp/srp.js";
-import { getParams } from "../src/srp/params.js";
-import * as utils from "../src/utils.js";
+import { getParams, getTestVectors } from "../src/srp/params.js";
 
-// User data that would be stored in the server's database
-const test_user = {
-  I: Buffer.from("alice"),
-  s: utils.hex.toBuffer("BEB25379 D1A8581E B5A72767 3A2441EE"),
-};
-
-const session = {
-  A: utils.hex.toBigInt(`
-    61D5E490 F6F1B795 47B0704C 436F523D D0E560F0 C64115BB 72557EC4
-    4352E890 3211C046 92272D8B 2D1A5358 A2CF1B6E 0BFCF99F 921530EC
-    8E393561 79EAE45E 42BA92AE ACED8251 71E1E8B9 AF6D9C03 E1327F44
-    BE087EF0 6530E69F 66615261 EEF54073 CA11CF58 58F0EDFD FE15EFEA
-    B349EF5D 76988A36 72FAC47B 0769447B
-  `),
-  B: utils.hex.toBigInt(`
-    BD0C6151 2C692C0C B6D041FA 01BB152D 4916A1E7 7AF46AE1 05393011
-    BAF38964 DC46A067 0DD125B9 5A981652 236F99D9 B681CBF8 7837EC99
-    6C6DA044 53728610 D0C6DDB5 8B318885 D7D82C7F 8DEB75CE 7BD4FBAA
-    37089E6F 9C6059F3 88838E7A 00030B33 1EB76840 910440B1 B27AAEAE
-    EB4012B7 D7665238 A8E3FB00 4B117B58
-  `),
-  S: utils.hex.toBigInt(`
-    B0DC82BA BCF30674 AE450C02 87745E79 90A3381F 63B387AA F271A10D
-    233861E3 59B48220 F7C4693C 9AE12B0A 6F67809F 0876E2D0 13800D6C
-    41BB59B6 D5979B5C 00A172B4 A2A5903A 0BDCAF8A 709585EB 2AFAFA8F
-    3499B200 210DCC1F 10EB3394 3CD67FC8 8A2F39A4 BE5BEC4E C0A3212D
-    C346D7E4 74B29EDE 8A469FFE CA686E5A
-  `),
-};
+const test_vectors = getTestVectors();
+const N = getParams(1024).N;
 
 describe("Core SRP cryptography", () => {
-  test("Derive k", () => {
-    const actual = core.derive_k();
-    const expected =
-      utils.hex.toBigInt("7556AA04 5AEF2CDD 07ABAF0F 665C3E81 8913186F");
-
-    expect(actual).toBe(expected);
+  test("Derive multiplier parameter (k)", () => {
+    const k = core.derive_k();
+    expect(k).toBe(test_vectors.k);
   });
 
-  test("Derive u", () => {
-    const actual = core.derive_u(session.A, session.B);
-    const expected =
-      utils.hex.toBigInt("CE38B959 3487DA98 554ED47D 70A7AE5F 462EF019");
-
-    expect(actual).toBe(expected);
+  test("Derive random scrambling parameter (u)", () => {
+    const u = core.derive_u(test_vectors.A, test_vectors.B);
+    expect(u).toBe(test_vectors.u);
   });
 
-  test("Derive K (session key)", () => {
-    const actual = utils.hex.toBigInt(core.derive_K(session.S));
-    const expected =
-      utils.hex.toBigInt("017EEFA1 CEFC5C2E 626E2159 8987F31E 0F1B11BB");
-
-    expect(actual).toBe(expected);
+  test("Derive session key (K)", () => {
+    const K = core.derive_K(test_vectors.S);
+    expect(K).toBe(test_vectors.K);
   });
 });
 
 describe("Client-side SRP cryptography", () => {
-  const p = Buffer.from("password123");
-  const a = utils.hex.toBigInt(
-    "60975527 035CF2AD 1989806F 0407210B C81EDC04 E2762A56 AFD529DD DA2D4393"
-  );
-
-  test("Derive x", () => {
-    const actual = client.derive_x(test_user.I, p, test_user.s);
-    const expected =
-      utils.hex.toBigInt("94B7555A ABE9127C C58CCF49 93DB6CF8 4D16C124");
-
-    expect(actual).toBe(expected);
+  test("Derive private key (x)", () => {
+    const x = client.derive_x(test_vectors.I, test_vectors.p, test_vectors.s);
+    expect(x).toBe(test_vectors.x);
   });
 
-  test("Derive v (verifier)", () => {
-    const x = client.derive_x(test_user.I, p, test_user.s);
-    const actual = client.derive_v(x);
-    test_user.v = actual;
-
-    const expected = utils.hex.toBigInt(`
-      7E273DE8 696FFC4F 4E337D05 B4B375BE B0DDE156 9E8FA00A 9886D812
-      9BADA1F1 822223CA 1A605B53 0E379BA4 729FDC59 F105B478 7E5186F5
-      C671085A 1447B52A 48CF1970 B4FB6F84 00BBF4CE BFBB1681 52E08AB5
-      EA53D15C 1AFF87B2 B9DA6E04 E058AD51 CC72BFC9 033B564E 26480D78
-      E955A5E2 9E7AB245 DB2BE315 E2099AFB
-    `);
-
-    expect(actual).toBe(expected);
+  test("Derive verifier (v)", () => {
+    const v = client.derive_v(test_vectors.x);
+    expect(v).toBe(test_vectors.v);
   });
 
-  test("Derive A", () => {
-    expect(client.derive_A(a)).toBe(session.A);
+  test("Derive public ephemeral key (A)", () => {
+    const A = client.derive_A(test_vectors.a);
+    expect(A).toBe(test_vectors.A);
   });
 
-  test("Derive S", () => {
+  test("Derive unhashed session key (S)", () => {
     const S = client.derive_S(
-      core.derive_k(),
-      client.derive_x(test_user.I, p, test_user.s),
-      a,
-      session.B,
-      core.derive_u(session.A, session.B)
+      test_vectors.k,
+      test_vectors.x,
+      test_vectors.a,
+      test_vectors.B,
+      test_vectors.u
     );
 
-    expect(S).toBe(session.S);
+    expect(S).toBe(test_vectors.S);
   });
 
-  test("Abort with invalid server ephemeral", () => {
+  test("Abort with invalid server ephemeral (B)", () => {
     expect(() => {
       client.derive_S(
-        core.derive_k(),
-        client.derive_x(test_user.I, p, test_user.s),
-        a,
-        getParams(1024).N * 2n,
-        core.derive_u(session.A, session.B)
+        test_vectors.k,
+        test_vectors.x,
+        test_vectors.a,
+        N * 2n,
+        test_vectors.u
       );
     }).toThrowError("abort");
 
     expect(() => {
       client.derive_S(
-        core.derive_k(),
-        client.derive_x(test_user.I, p, test_user.s),
-        a,
-        session.B,
+        test_vectors.k,
+        test_vectors.x,
+        test_vectors.a,
+        test_vectors.B,
         0n
       );
     }).toThrowError("abort");
   });
 
-  test("Derive M1", () => {
-    const actual = client.derive_M1(
-      test_user.I, test_user.s,
-      utils.hex.toBuffer(session.A),
-      utils.hex.toBuffer(session.B),
-      core.derive_K(session.S)
+  test("Derive auth challenge (M1)", () => {
+    const M1 = client.derive_M1(
+      test_vectors.I,
+      test_vectors.s,
+      test_vectors.A,
+      test_vectors.B,
+      test_vectors.K
     );
-    const expected =
-      utils.hex.toBigInt("3F3BC671 69EA7130 2599CF1B 0F5D408B 7B65D347");
 
-    session.M1 = actual;
-    expect(actual).toBe(expected);
+    expect(M1).toBe(test_vectors.M1);
   });
 });
 
 describe("Server-side SRP cryptography", () => {
-  const b = utils.hex.toBigInt(
-    "E487CB59 D31AC550 471E81F0 0F6928E0 1DDA08E9 74A004F4 9E61F5D1 05284D20"
-  );
-
-  test("Derive B", () => {
-    expect(
-      server.derive_B(b, test_user.v, core.derive_k())
-    ).toBe(session.B);
+  test("Derive public ephemeral key (B)", () => {
+    const B = server.derive_B(test_vectors.b, test_vectors.v, test_vectors.k);
+    expect(B).toBe(test_vectors.B);
   });
 
-
-  test("Derive S", () => {
+  test("Derive unhashed session key (S)", () => {
     const S = server.derive_S(
-      test_user.v,
-      session.A,
-      b,
-      core.derive_u(session.A, session.B)
+      test_vectors.v,
+      test_vectors.A,
+      test_vectors.b,
+      test_vectors.u
     );
 
-    expect(S).toBe(session.S);
+    expect(S).toBe(test_vectors.S);
   });
 
-  test("Abort with invalid client ephemeral", () => {
+  test("Abort with invalid client ephemeral (A)", () => {
     expect(() => {
-      server.derive_S(
-        test_user.v,
-        getParams(1024).N * 2n,
-        b,
-        core.derive_u(session.A, session.B)
-      );
+      server.derive_S(test_vectors.v, N * 2n, test_vectors.b, test_vectors.u);
     }).toThrowError("abort");
   });
 
-  test("Derive M2", () => {
-    const actual = server.derive_M2(
-      utils.hex.toBuffer(session.A),
-      utils.hex.toBuffer(session.M1),
-      utils.hex.toBuffer(core.derive_K(session.S))
+  test("Derive auth challenge (M2)", () => {
+    const M2 = server.derive_M2(
+      test_vectors.A,
+      test_vectors.M1,
+      test_vectors.K
     );
-    const expected =
-      utils.hex.toBigInt("9CAB3C57 5A11DE37 D3AC1421 A9F00923 6A48EB55");
 
-    expect(actual).toBe(expected);
+    expect(M2).toBe(test_vectors.M2);
   });
 });
-
