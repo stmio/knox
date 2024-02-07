b9b2ae5ca21c8f06cd0b4570149d00eabcc09eaa
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-02-07 15:39:22 +0000

    feat(redis): add for server login cache

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,12 @@
 import ViteExpress from "vite-express";
 import app from "./app.js";
 import { initDatabase } from "./config/database.js";
+import { startRedis } from "./config/redis.js";
 
 const PORT = 3000;
 
 ViteExpress.listen(app, PORT, async () => {
-  console.log("Server is listening on port", PORT);
   await initDatabase();
+  await startRedis();
+  console.log("Server is listening on port", PORT);
 });
