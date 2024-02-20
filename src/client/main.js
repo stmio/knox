ed24db19cec14951dd415fb4f3d14ac1a841576c
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-02-20 12:10:45 +0000

    feat(pages): update homepage layout and content

--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,12 @@
 import "./style.css";
 import knoxLogo from "/knox.svg";
+import authIcon from "/mutual.svg";
+import encryptedIcon from "/no-search.svg";
+import signedIcon from "/signature.svg";
+import transportIcon from "/van.svg";
 
-document.querySelector(".logo").src = knoxLogo;
+document.getElementById("knox").src = knoxLogo;
+document.getElementById("comms").src = transportIcon;
+document.getElementById("encrypted").src = encryptedIcon;
+document.getElementById("auth").src = authIcon;
+document.getElementById("signed").src = signedIcon;
