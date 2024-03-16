9a39e79526817ac5824a87c33ccedfdcc97ee3f0
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-03-16 21:41:50 +0000

    feat(auth): verify server responses with hmac signature

--- src/client/scripts/middleware.js
+++ src/client/scripts/middleware.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { signRequest } from "./keys.js";
+import { signRequest, verifyResponse } from "./keys.js";
 
 axios.interceptors.request.use(
   (config) => {
@@ -16,6 +16,30 @@ axios.interceptors.request.use(
     config.headers["X-Request-Signature"] = signature;
 
     return config;
+  },
+  (error) => {
+    return Promise.reject(new Error(error));
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    const timestamp = res.headers["x-response-timestamp"];
+    const signature = res.headers["x-response-signature"];
+
+    const validSignature = verifyResponse(
+      signature,
+      res.config.method,
+      res.config.url,
+      timestamp,
+      res.data
+    );
+
+    if (!validSignature)
+      return Promise.reject(new Error("Server sent an invalid signature"));
+    else if (Math.floor(new Date().getTime() / 1000) - timestamp > 5)
+      return Promise.reject(new Error("Response timestamp is too old"));
+    else return res;
   },
   (error) => {
     return Promise.reject(new Error(error));
